814ad1fa12b7e6b334d1b3cb1936275e88701f92
paulo-e-garcia <14171755+paulo-e-garcia@users.noreply.github.com>
2026-01-16 20:55:09 +0000

    Uploading code

---
+++ yolo-to-onnx.py
@@ -0,0 +1,19 @@
+import torch
+from ultralytics import YOLO
+
+# Load model
+model = YOLO('yolov8m.pt')
+
+# Export to ONNX format
+model.export(
+    format='onnx',
+    imgsz=640,
+    device='cuda:0',
+    half=False,  # Use FP32
+    int8=False,
+    dynamic=False,
+    simplify=True,
+    opset=11,
+    data=None,
+    verbose=True
+)
